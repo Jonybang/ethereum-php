243c9f532e4970764ab80dd61e8ac04abf1b4535
jonybang <jonybange@gmail.com>
2018-03-10 20:48:05 +0200

    handle sendTransaction error

--- src/EthereumPHP/Methods/Eth.php
+++ src/EthereumPHP/Methods/Eth.php
@@ -181,7 +181,7 @@ class Eth extends AbstractMethods
         try {
             return new TransactionHash($response->getRpcResult());
         } catch (\LengthException $exception){
-            throw new \ErrorException($response->getRpcResult());
+            throw new \ErrorException($response->getRpcErrorCode() . ": " . $response);
         }
     }
 
